524952733ec5f85148138d81d5a70d5965b5869a
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-05-12 14:31:12 +0430

    cleaning the code

--- preprocess1.py
+++ preprocess1.py
@@ -2,21 +2,35 @@ from skimage.io import imread
 from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
 from skimage.transform import rotate
+from skimage import measure
+from skimage.measure import regionprops
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+# import cv2
+
+
+def rotate_image(inital_image):
+    return rotate(inital_image, -11, center=None)
+
+
+def binarization(rotated_image):
+    threshold_value = threshold_otsu(rotated_image)
+    return rotated_image > threshold_value
+
 
 
 car_image = imread("2454.jpg", as_grey=True)
-# it should be a 2 dimensional array
+gray_car_image = car_image * 255
+rotated_image = rotate_image(gray_car_image)
 print(car_image.shape)
 
 # the next line is not compulsory however, a grey scale pixel
 # in skimage ranges between 0 & 1. multiplying it with 255
 # will make it range between 0 & 255 (something we can relate better with
 
-gray_car_image = car_image * 255
-gray_car_image = rotate(gray_car_image,-11,center=None)
+
 fig, (ax1, ax2) = plt.subplots(1, 2)
-ax1.imshow(gray_car_image, cmap="gray")
-threshold_value = threshold_otsu(gray_car_image)
-binary_car_image = gray_car_image > threshold_value
-ax2.imshow(binary_car_image, cmap="gray")
+ax1.imshow(binarization(rotated_image), cmap="gray")
+ax2.imshow(, cmap="gray")
 plt.show()
+
