c923f7b87216234bb4cd368b32ff3392b083aaf7
Mahdi Sadeghzadeh Ghamsary <mahdisadeghzadeh24@gmail.com>
2019-05-21 03:24:58 +0430

    add a way to find english numbers

--- preprocess1.py
+++ preprocess1.py
@@ -4,19 +4,21 @@ import matplotlib.pyplot as plt
 from skimage.transform import rotate
 from skimage import measure
 from skimage.measure import regionprops
-import matplotlib.pyplot as plt
 import matplotlib.patches as patches
-# import cv2
+import numpy as np
+from skimage.transform import resize
+import cv2
+from skimage import img_as_ubyte
+
 
 
 def rotate_image(inital_image):
     return rotate(inital_image, -11, center=None)
 
 
-def binarization(rotated_image):
-    threshold_value = threshold_otsu(rotated_image)
-    return rotated_image > threshold_value
-
+def binarization(input_image):
+    threshold_value = threshold_otsu(input_image)
+    return input_image > threshold_value
 
 
 car_image = imread("2454.jpg", as_grey=True)
@@ -51,26 +53,54 @@ for region in regionprops(label_image):
         continue
 
     # the bounding box coordinates
-    # minRow, minCol, maxRow, maxCol = region.bbox
-    # rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="red", linewidth=2, fill=False)
-    # ax1.add_patch(rectBorder)
+    minRow, minCol, maxRow, maxCol = region.bbox
+    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="green", linewidth=1, fill=False)
+    ax1.add_patch(rectBorder)
     # let's draw a red rectangle over those regions
 
-    
     min_row, min_col, max_row, max_col = region.bbox
     region_height = max_row - min_row
     region_width = max_col - min_col
     # print(region_height,region_width)
     # ensuring that the region identified satisfies the condition of a typical license plate
     if region_height >= min_height and region_height <= max_height and region_width >= min_width and region_width <= max_width and region_width > region_height:
-        print(region_height,region_width)
+        print(region_height, region_width)
         plate_like_objects.append(gray_car_image[min_row:max_row,
                                   min_col:max_col])
-        plate_objects_cordinates.append((min_row, min_col,
-                                              max_row, max_col))
+        plate_objects_cordinates.append((min_row, min_col, max_row, max_col))
         rectBorder = patches.Rectangle((min_col, min_row), max_col-min_col, max_row-min_row, edgecolor="red", linewidth=2, fill=False)
         ax1.add_patch(rectBorder)
     # let's draw a red rectangle over those regions
 
 plt.show()
 
+for plate in plate_like_objects:
+    binary_plate = binarization(plate)
+    license_plate = np.invert(binary_plate)
+
+    cv_image = img_as_ubyte(license_plate)
+    img = cv_image
+
+    # find contours
+    ctrs, hier = cv2.findContours(cv_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # sort contours
+    sorted_ctrs = sorted(ctrs, key=lambda ctr: cv2.boundingRect(ctr)[0])
+
+    for i, ctr in enumerate(sorted_ctrs):
+        # Get bounding box
+        x, y, w, h = cv2.boundingRect(ctr)
+
+        # Getting ROI
+        roi = img[y:y+h, x:x+w]
+
+        # show ROI
+        # cv2.imwrite('roi_imgs.png', roi)
+        # cv2.imshow('charachter'+str(i), roi)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (90, 0, 255), 2)
+        # cv2.waitKey(0)
+        # cv2.destroyAllWindows()
+    print('found')
+    cv2.imshow('marked areas', img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
